e91df9a2f04bb41eb4f884a40edfc6048d6ee3aa
aminebenjebli <80284883+aminebenjebli@users.noreply.github.com>
2025-02-04 22:22:04 +0100

    fix containers in the screen

--- healthapp/lib/input_page.dart
+++ healthapp/lib/input_page.dart
@@ -9,14 +9,44 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to BMI Calculator',
-          style: TextStyle(fontSize: 24),
+        appBar: AppBar(
+          title: const Text('BMI CALCULATOR'),
         ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                ),
+                Expanded(child: ReusableCard(colour: const Color(0xFF1D1E33))),
+              ],
+            )),
+            Expanded(child: ReusableCard(colour: const Color(0xFF1D1E33))),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(colour: const Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(colour: const Color(0xFF1D1E33))),
+              ],
+            ))
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour});
+  Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
